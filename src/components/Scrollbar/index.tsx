ad8e82f28dc9bb20c97a0ce8e26414b1be937617
Dinhhai2002 <88237144+Dinhhai2002@users.noreply.github.com>
2023-12-16 16:08:42 +0700

    update

--- src/components/Scrollbar/index.tsx
+++ src/components/Scrollbar/index.tsx
@@ -1,13 +1,13 @@
 import { FC, ReactNode } from 'react';
 import PropTypes from 'prop-types';
-import { ScrollbarProps, Scrollbars } from 'react-custom-scrollbars-2';
+import { Scrollbars } from 'react-custom-scrollbars-2';
 
 import { Box, useTheme } from '@mui/material';
 
-// interface ScrollbarProps {
-//   className?: string;
-//   children?: ReactNode;
-// }
+interface ScrollbarProps {
+  className?: string;
+  children?: ReactNode;
+}
 
 const Scrollbar: FC<ScrollbarProps> = ({ children, ...rest }) => {
   const theme = useTheme(); // Ensure useTheme() is imported correctly
@@ -35,9 +35,9 @@ const Scrollbar: FC<ScrollbarProps> = ({ children, ...rest }) => {
   );
 };
 
-Scrollbar.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string
-};
+// Scrollbar.propTypes = {
+//   children: PropTypes.node,
+//   className: PropTypes.string
+// };
 
 export default Scrollbar;
